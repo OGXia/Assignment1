0d8973e6eaf2871fac7aa66fd67f06c34861b051
unknown <notpixi986@gmail.com>
2026-01-19 20:46:36 +0530

    task completed

---
+++ Task1.py
@@ -0,0 +1,11 @@
+firstnumber = int(input("Enter the first number: "))
+secondnumber = int(input("Enter the second number: "))
+sum = firstnumber + secondnumber
+sub = firstnumber - secondnumber
+mul = firstnumber * secondnumber    
+div = firstnumber / secondnumber
+print("The sum of  two numbers is :" , sum)
+print("The subtraction of two numbers is :" , sub)
+print("The multiplication of two numbers is :" , mul)        
+print("The division of two numbers is :" , div)
+
---
+++ Task2.py
@@ -0,0 +1,4 @@
+firstName = input("enter your first name: ")
+lastName = input("enter your last name: ")
+finalName = (firstName+lastName)
+print("Hello",finalName,"! welcome to our studio")
